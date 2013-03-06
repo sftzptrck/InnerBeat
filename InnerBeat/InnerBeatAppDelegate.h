1a14fa1e4f8742ae36c7fc36580e1ee7a12ce532
Shawn Fitzpatrick <shawn@Shawns-MacBook-Pro.local>
2013-03-05 20:48:05 -0800

    Initial Commit

---
+++ InnerBeat/InnerBeatAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  InnerBeatAppDelegate.h
+//  InnerBeat
+//
+//  Created by Shawn Fitzpatrick on 3/5/13.
+//  Copyright (c) 2013 Shawn Fitzpatrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InnerBeatAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InnerBeat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InnerBeat
+//
+//  Created by Shawn Fitzpatrick on 3/5/13.
+//  Copyright (c) 2013 Shawn Fitzpatrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "InnerBeatAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([InnerBeatAppDelegate class]));
+    }
+}
