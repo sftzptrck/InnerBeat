e84747501c3f46177c66149a81f1908a1ee183e5
Shawn Fitzpatrick <shawn@Shawns-MacBook-Pro.local>
2013-03-06 21:51:31 -0800

    first commit

---
+++ InnerBeat/PlaylistSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaylistSelectViewController.h
+//  InnerBeat
+//
+//  Created by Shawn Fitzpatrick on 3/5/13.
+//  Copyright (c) 2013 Shawn Fitzpatrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaylistSelectViewController : UIViewController
+
+@end
---
+++ InnerBeat/ProfileSelectViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileSelectViewController.h
+//  InnerBeat
+//
+//  Created by Shawn Fitzpatrick on 3/5/13.
+//  Copyright (c) 2013 Shawn Fitzpatrick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileSelectViewController : UIViewController
+
+@end
