b74e0b44416f087cb1c30e1c4b36a0ede9862a30
Shawn Fitzpatrick <shawn.fitzpat@gmail.com>
2013-03-11 23:15:44 -0700

    Hooked up components of ProfileEditViewController

--- InnerBeat/ProfileEditViewController.h
+++ InnerBeat/ProfileEditViewController.h
@@ -7,7 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ProfileItem.h"
 
 @interface ProfileEditViewController : UIViewController
-
+{
+    __weak IBOutlet UITextField *profileName;
+    __weak IBOutlet UITextField *targetPace;
+    __weak IBOutlet UITextField *minuteChangeDiff;
+    __weak IBOutlet UITextField *secondChangeDiff;
+    __weak IBOutlet UISlider *sensitivity;
+}
+@property (nonatomic, strong) ProfileItem *item;
 @end
--- InnerBeat/ProfileEditViewController.m
+++ InnerBeat/ProfileEditViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ProfileEditViewController.h"
+#import "ProfileItem.h"
 
 @interface ProfileEditViewController ()
 
@@ -14,25 +15,50 @@
 
 @implementation ProfileEditViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+@synthesize item;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    [[self view] setBackgroundColor:[UIColor groupTableViewBackgroundColor]];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)viewWillAppear:(BOOL)animated
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [super viewWillAppear:animated];
+    [profileName setText:[item profileName]];
+    [targetPace setText:[NSString stringWithFormat:@"%d:%d", [item targetPaceMinutes], [item targetPaceSeconds]]];
+    [minuteChangeDiff setText:[NSString stringWithFormat:@"%d",[item tempoAllowMinutes]]];
+    [secondChangeDiff setText:[NSString stringWithFormat:@"%d",[item tempoAllowSeconds]]];
+    [sensitivity setValue:[item tempoSensitivity]];
+    
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    // Clear first responder
+    [[self view] endEditing:YES];
+    // "Save" changes to item
+    [item setProfileName:[profileName text]];
+    
+    
+    NSArray *targetPaceComponents = [[targetPace text] componentsSeparatedByString: @":"];
+    
+    int tempMinutes = [(NSString*)[targetPaceComponents objectAtIndex:0] intValue];
+    int tempSeconds = [(NSString*)[targetPaceComponents objectAtIndex:1] intValue];
+    
+    [item setTargetPaceMinutes:tempMinutes];
+    [item setTargetPaceSeconds:tempSeconds];
+    [item setTempoAllowMinutes:[[minuteChangeDiff text] intValue]];
+    [item setTempoAllowSeconds:[[secondChangeDiff text] intValue]];
+}
+
+- (void)setItem:(ProfileItem *)i
+{
+    item = i;
+    [[self navigationItem] setTitle:[item profileName]];
+    
 }
 
 @end
